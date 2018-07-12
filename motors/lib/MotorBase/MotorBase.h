602756c13fc560d189228c6dceac85c078bfb872
David Berard <davidberard98@gmail.com>
2018-07-11 20:50:13 -0700

    pid testing

--- motors/lib/MotorBase/MotorBase.h
+++ motors/lib/MotorBase/MotorBase.h
@@ -7,8 +7,8 @@
  *
  */
 
-constexpr int8_t highPwmPins[] = {PA0, PA6};
-constexpr int8_t lowPwmPins[] = {PA1, PA7};
+constexpr int8_t highPwmPins[] = {PB6, PA6};
+constexpr int8_t lowPwmPins[] = {PB7, PA7};
 
 static_assert(sizeof highPwmPins == sizeof lowPwmPins);
 
