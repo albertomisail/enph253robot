64257050045416739d76362f7991ce17e05b1bb5
Alberto Misail <ajmisailguber@gmail.com>
2018-08-02 15:02:17 -0700

    Handle second wok code

--- main/lib/Constants/Constants.h
+++ main/lib/Constants/Constants.h
@@ -61,6 +61,6 @@ public:
 
     constexpr static uint8_t MULTIPLEXER_PIN = PB5;
     constexpr static uint8_t BRIDGE_SERVO_PIN = PA3;
-    constexpr static int16_t positionLock = 0; //TODO change this
+    constexpr static int16_t positionLock = 180; //TODO change this
     constexpr static int16_t positionUnlock = 90;
 };
