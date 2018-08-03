ba852ee63112eeb7287adc69155497d0d9ffc90d
David Berard <davidberard98@gmail.com>
2018-08-02 23:36:17 -0700

    Initial test setup with CMake and googletest

---
+++ main/test/mock/Arduino.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "defines.h"
+#include "ADC.h"
+#include "Servo.h"
---
+++ main/test/mock/defines.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "defines.h"
+#include "ADC.h"
+#include "Arduino.h"
+#include "Servo.h"
